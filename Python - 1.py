c7fc8ab20c6134c61ca8f4e4617e7b36de8d2946
Mansihub <77737060+Mansihub@users.noreply.github.com>
2021-02-23 11:08:54 +0530

    Day 1

---
+++ Python - 1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Taking two numbers as input and print the result of addition,subtraction,multiplication and division
+
+# In[7]:
+
+
+x=int(input())
+y=int(input())
+print ( "sum=", x+y)
+print ("subtraction=",x-y)
+print ("multiplication=",x*y)
+print ("division=",x/y )
+
+
+# # Taking two numbers X and Y as input and printing result of X to the power Y (X^Y)
+# 
+
+# In[8]:
+
+
+x=int(input())
+y=int(input())
+print (x**y)
+
+
+# In[ ]:
+
+
+
+
